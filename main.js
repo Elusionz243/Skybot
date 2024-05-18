a156ab3d8116422e9ce5aa0b8dab951d8d8d0653
Elusionz243 <strivelinkband@yahoo.com>
2024-05-18 14:04:20 -0500

    Added menus and submenus, working on generating menus automatically

--- main.js
+++ main.js
@@ -1,102 +1,71 @@
-const { Api, Bot, Keyboard, InlineKeyboard } = require("grammy");
-// const http = require("http");
+const { Bot } = require("grammy");
+const { Menu } = require("@grammyjs/menu");
 require("dotenv").config();
-// const xmlRequest = new XMLHttpRequest();
 
 const { TEST_BOT_TOKEN } = process.env;
 
+// Load Menu Options
+const trainingMenuButtons = require("./utils/training.json");
+const humanResourcesMenuButtons = require("./utils/hr.json");
+
 const bot = new Bot(TEST_BOT_TOKEN);
-const listOfCommands = [
-  ["/training", "Training"],
-  ["/humanResources", "Human Resources"],
-];
 
-const listOfButtons = {
-  Glass: {
-    "Water Pipes": [
-      "Most Common Water Pipe Types (future HSO blog post)",
-      "Oil burners",
-    ],
-    "Water Pipe Accessories": [
-      "Bangers",
-      "Percolators",
-      "Downstems",
-      "Ash Catchers",
-    ],
-    "Dry Pipes": ["Hand Pipes", "Chillums", "Nectar Collectors"],
-  },
-  Kratom: {
-    "General Knowledge": [
-      "Common Uses",
-      "Medical Benefits",
-      "Strain Genetics",
-      "Vein Colors",
-      "Dosage Guidance",
-      "Alkaloids",
-      "Common Drug Interaction",
-    ],
-    "Common Product Types": ["Whole Herb", "Extracts", "Edibles & Drinks"],
-  },
-  Hemp: {
-    "General Knowledge": [
-      "2018 Farm Bill",
-      "Sativas vs Indicas",
-      "Hemp vs Cannabis",
-      "Non-Psychoactive Cannabinoids",
-      "Psychoactive Cannabinoids",
-      "Terpenes",
-    ],
-    "Common Product Types": [
-      "Flower",
-      "Cartridges",
-      "Edibles",
-      "Concentrates",
-      "Disposables",
-    ],
-    FADE: [],
-  },
-  Vapes: {
-    "Disposable Vapes": ["Lost Mary", "Elf Bar", "Geek Bar", "Raz (TN9000)"],
-    "Reuseable Vapes": [
-      "Mods & Kits",
-      "Pods, Tanks, & Coils",
-      "Batteries",
-      "Troubleshooting Tips",
-    ],
-  },
-  Supplements: {
-    "Detox Products": [],
-    "Inositol & Lactose": [],
-  },
-  "Hookah & Shisha": {
-    "Hookah Lore": [],
-    "Hookah Maintenence": [],
-    Shisha: [],
-  },
-};
+// Load Menu
+const mainMenu = new Menu("root-menu")
+  .submenu("Training", "training-menu")
+  .submenu("Human Resources", "human-resources-menu");
 
-const generateKeyboard = async (ctx, categories) => {
-  const keyboard = new Keyboard();
-  for (const category of categories) {
-    keyboard.text(category);
+const trainingMenu = new Menu("training-menu")
+  .submenu("Glass", "glass-menu")
+  .row()
+  .submenu("Kratom", "kratom-menu")
+  .row()
+  .submenu("Hemp", "hemp-menu")
+  .row()
+  .submenu("Vapes", "vapes-menu")
+  .row()
+  .submenu("Supplements", "supplements-menu")
+  .row()
+  .submenu("Hookah & Shisha", "hookah-menu")
+  .row()
+  .back("Go Back");
+
+const humanResourcesMenu = new Menu("human-resources-menu")
+  .text("Benefits", (ctx) => ctx.reply("www.Dontclickme.com"))
+  .row()
+  .text("Employee Handbook", (ctx) => ctx.reply("www.Dontclickme.com/2"))
+  .row()
+  .text("Organization Chart", (ctx) => ctx.reply("www.Dontclickme.com/3"))
+  .row()
+  .text("Hazel History", (ctx) => ctx.reply("www.Dontclickme.com/4"))
+  .row()
+  .back("Go Back");
+/**
+ * Generates menu options for the main menu.
+ * @param {Object} ctx - The context object.
+ * @param {Array} menuButtons - The array of menu buttons.
+ * @returns {Promise<void>}
+ */
+
+const generateSubMenu = (menuButtons) => {
+  // Iterate over the menu buttons and add them to the main menu
+  for (let i = 0; i < menuButtons.length; i++) {
+    const category = menuButtons[i];
+
+    // Add a new menu option for the category title
+    mainMenu.text(category.title, (ctx) => ctx.reply(category.title)).row();
   }
-  ctx.reply("Please select a category ⬇️", {
-    reply_markup: keyboard,
-  });
+
 };
 
-const startKeyboard = new InlineKeyboard()
-  .text("Training")
-  .text("Human Resources");
+mainMenu.register(trainingMenu);
+mainMenu.register(humanResourcesMenu);
 
-const menuOptions = (ctx) => {
-  ctx.reply("Please select an option to begin ⬇️", {
-    reply_markup: startKeyboard,
-  });
-};
+bot.use(mainMenu);
 
-bot.command("start", (ctx) => menuOptions(ctx));
-bot.command("training", (ctx) => generateKeyboard(ctx, listOfButtons));
+bot.command("start", (ctx) =>
+  ctx.reply("Select an option from below 👇", { reply_markup: mainMenu })
+);
 
 bot.start();
 console.log("Bot started");
